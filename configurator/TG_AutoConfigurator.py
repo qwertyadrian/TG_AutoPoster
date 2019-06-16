40832d7e1c81e97630d26fd234633b091fcfd9bf
qwertyadrian <qwertyadrian@yandex.ru>
2019-06-16 18:27:09 +0400

    Теперь бот TG_AutoConfigurator работает в режиме демона (только в Linux). Добавлена команда /kill для завершения работы бота TG_AutoConfigurator, если он был запущен в режиме демона. В Windows она ничего не делает. Подготовка к переносу бота TG_AutoConfigurator в отдельный репозиторий.

--- configurator/TG_AutoConfigurator.py
+++ configurator/TG_AutoConfigurator.py
@@ -1,12 +1,12 @@
 import configparser
 import telebot
-# import daemon
+import sys
 import commands
 from loguru import logger
 
 
 def main():
-    # Чтение конфигурации бота из файла config.ini.example
+    # Чтение конфигурации бота из файла config.ini
     config = configparser.ConfigParser()
     config.read_file(open('config.ini', 'r', encoding='utf-8'))
     # Инициализация Telegram бота
@@ -23,7 +23,16 @@ def main():
 
 
 if __name__ == '__main__':
-    # with daemon.DaemonContext(working_directory='.'):
-    #     logger.add('./logs/bot_log_{time}.log')
-    main = logger.catch(main)
-    main()
+    if sys.platform == 'linux':
+        import daemon
+        import daemon.pidfile
+
+        with daemon.DaemonContext(working_directory='.', pidfile=daemon.pidfile.PIDLockFile('TG_AutoConfigurator.pid')):
+            logger.add('./logs/bot_log_{time}.log')
+            logger.info('Запуск бота в режиме демона. PID процесса хранится в файле TG_AutoConfigurator.pid')
+            main = logger.catch(main)
+            main()
+    else:
+        logger.add('./logs/bot_log_{time}.log')
+        main = logger.catch(main)
+        main()
