ab983b5aadef8d6a434dd040d28059115cc8abf4
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2020-01-03 19:43:52 +0300

    Теперь возможно указать свой путь к папке для кэша По умолчанию используется временная папка /tmp (Linux) или %TEMP% (Windows) Уменьшено количество обращений к файлу конфигурации Обновлен TG_AutoPoster.py Обновлен parser.py

--- TG_AutoPoster.py
+++ TG_AutoPoster.py
@@ -7,12 +7,18 @@ from vk_api import VkApi
 from handlers import *
 from parser import get_posts
 from sender import PostSender
-from os import chdir, listdir, remove, mkdir
 from loguru import logger as log
 from datetime import timedelta
 from time import sleep
+from tempfile import TemporaryDirectory
+import os.path
 import configparser
 import argparse
+
+CACHE_DIR = TemporaryDirectory(prefix='TG_AutoPoster')
+CONFIG_PATH = os.path.join(os.getcwd(), 'config.ini')
+
+log.add('./logs/bot_log_{time}.log', retention=timedelta(days=2))
 
 
 def create_parser():
@@ -20,96 +26,87 @@ def create_parser():
                                      description='Telegram Bot for AutoPosting from VK',
                                      epilog='(C) 2018-2020 Adrian Polyakov\nReleased under the MIT License.')
 
-    parser.add_argument('-l', '--loop', action='store_const', const=True,
+    parser.add_argument('-l', '--loop', action='store_const', const=True, default=False,
                         help='Запустить бота в бесконечном цикле с проверкой постов раз в час (по умолчанию)')
     parser.add_argument('-s', '--sleep', type=int, default=0,
                         help='Проверять новые посты каждые N секунд', metavar='N')
-    parser.add_argument('-c', '--config', default='config.ini',
-                        help='Путь к конфиг файлу бота (по умолчанию ./config.ini)')
+    parser.add_argument('-c', '--config', default=CONFIG_PATH,
+                        help='Абсолютный путь к конфиг файлу бота (по умолчанию {})'.format(CONFIG_PATH))
+    parser.add_argument('--cache-dir', default=CACHE_DIR.name,
+                        help='Абсолютный путь к папке с кэшем бота (по умолчанию используется временная папка)')
     return parser
 
 
-cache_directory = '.cache'
-config = None
-bot = None
-stop_list = None
-api_vk = None
-session = None
-log.add('./logs/bot_log_{time}.log', retention=timedelta(days=2))
+class AutoPoster:
+    def __init__(self, config_path=CONFIG_PATH, cache_dir=CACHE_DIR.name):
+        self.cache_dir = cache_dir
+        self.config_path = config_path
+        # Чтение конфигурации бота из файла config.ini
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_path)
+        # Инициализация Telegram бота
+        bot_token = self.config.get('global', 'bot_token')
+        # Указан ли прокси в конфиге
+        if self.config.get('global', 'proxy_url'):
+            log.warning('Бот будет работать через прокси. Возможны перебои в работе бота.')
+            request = Request(proxy_url=self.config.get('global', 'proxy_url'), connect_timeout=15.0, read_timeout=15.0)
+        else:
+            request = None
+        self.bot = Bot(bot_token, request=request)
+        # Чтение из конфига логина и пароля ВК
+        vk_login = self.config.get('global', 'login')
+        vk_pass = self.config.get('global', 'pass')
+        # Чтение из конфига пути к файлу со стоп-словами
+        self.stop_list = self.config.get('global', 'stop_list', fallback=[])
+        if self.stop_list:
+            # Инициализация списка стоп-слов
+            with open(self.stop_list, 'r', encoding='utf-8') as f:
+                self.stop_list = [i.strip() for i in f.readlines()]
+        # Инициализация ВК сессии
+        self.session = VkApi(login=vk_login, password=vk_pass, auth_handler=auth_handler, captcha_handler=captcha_handler)
+        self.session.auth()
+        self.api_vk = self.session.get_api()
 
+    def get_updates(self):
+        # Переход в папку с кэшем
+        os.chdir(self.cache_dir)
+        for group in self.config.sections()[1:]:
+            last_id = self.config.getint(group, 'last_id', fallback=0)
+            pinned_id = self.config.getint(group, 'pinned_id', fallback=0)
+            # channel = config.get(group, 'channel', fallback=config.get('global', 'admin'))
+            # Получение постов
+            a = get_posts(group, last_id, pinned_id, self.api_vk, self.config, self.session)
+            for post in a:
+                skip_post = False
+                for word in self.stop_list:
+                    if word.lower() in post.text.lower():
+                        skip_post = True  # Если пост содержит стоп-слово, то пропускаем его.
+                        log.info('Пост содержит стоп-слово, поэтому он не будет отправлен.')
+                # Отправка постов
+                if not skip_post:
+                    sender = PostSender(self.bot, post, self.config.get(group, 'channel'))
+                    sender.send_post()
+        with open(self.config_path, 'w', encoding='utf-8') as f:
+            self.config.write(f)
+        for data in os.listdir('.'):
+            os.remove(data)
 
-def init(config_path='config.ini'):
-    global config, bot, stop_list, api_vk, session
-    # Чтение конфигурации бота из файла config.ini
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    # Инициализация Telegram бота
-    bot_token = config.get('global', 'bot_token')
-    # Указан ли прокси в конфиге
-    if config.get('global', 'proxy_url'):
-        log.warning('Бот будет работать через прокси. Возможны перебои в работе бота.')
-        request = Request(proxy_url=config.get('global', 'proxy_url'), connect_timeout=15.0, read_timeout=15.0)
-    else:
-        request = None
-    bot = Bot(bot_token, request=request)
-    # Чтение из конфига логина и пароля ВК
-    vk_login = config.get('global', 'login')
-    vk_pass = config.get('global', 'pass')
-    # Чтение из конфига пути к файлу со стоп-словами
-    stop_list = config.get('global', 'stop_list', fallback=[])
-    if stop_list:
-        # Инициализация списка стоп-слов
-        with open(stop_list, 'r', encoding='utf-8') as f:
-            stop_list = [i.strip() for i in f.readlines()]
-    # Инициализация ВК сессии
-    session = VkApi(login=vk_login, password=vk_pass, auth_handler=auth_handler, captcha_handler=captcha_handler)
-    session.auth()
-    api_vk = session.get_api()
-
-
-@log.catch(reraise=True)
-def main(config_path='../config.ini'):
-    # Переход в папку с кэшем
-    try:
-        chdir(cache_directory)
-    except FileNotFoundError:
-        log.exception('Директории кэша не существует. Создание...')
-        mkdir(cache_directory)
-        chdir(cache_directory)
-    for group in config.sections()[1:]:
-        last_id = config.getint(group, 'last_id', fallback=0)
-        pinned_id = config.getint(group, 'pinned_id', fallback=0)
-        # channel = config.get(group, 'channel', fallback=config.get('global', 'admin'))
-        # Получение постов
-        a = get_posts(group, last_id, pinned_id, api_vk, config, session, config_path)
-        for post in a:
-            skip_post = False
-            for word in stop_list:
-                if word.lower() in post.text.lower():
-                    skip_post = True  # Если пост содержит стоп-слово, то пропускаем его.
-                    log.info('Пост содержит стоп-слово, поэтому он не будет отправлен.')
-            # Отправка постов
-            if not skip_post:
-                sender = PostSender(bot, post, config.get(group, 'channel'))
-                sender.send_post()
-    for data in listdir('.'):
-        remove(data)
-    chdir('../')
+    def get_infinity_updates(self, interval=3600):
+        while True:
+            self.get_updates()
+            log.info('Работа завершена. Отправка в сон на {} секунд.'.format(sleep_time))
+            sleep(interval)
 
 
 if __name__ == '__main__':
     log.info('Начало работы.')
     cmd_parser = create_parser()
     namespace = cmd_parser.parse_args()
+    autoposter = AutoPoster(config_path=namespace.config, cache_dir=namespace.cache_dir)
     if namespace.loop or namespace.sleep:
         sleep_time = namespace.sleep if namespace.sleep else 3600
         log.info('Программе был передан аргумен --loop (-l). Запуск бота в бесконечном цикле.')
-        init(config_path=namespace.config)
-        while True:
-            main()
-            log.info('Работа завершена. Отправка в сон на {} секунд.'.format(sleep_time))
-            sleep(sleep_time)
+        autoposter.get_infinity_updates(interval=sleep_time)
     else:
-        init(config_path=namespace.config)
-        main()
+        autoposter.get_updates()
     log.info('Работа завершена. Выход...')
