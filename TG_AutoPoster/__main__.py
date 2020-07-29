9be9f568b5d1824bc134f7a8a2670fc51da502ef
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2020-07-29 12:31:55 +0400

    Добавлен параметр --ignore-errors для игнорирования необработанных ошибок (использовать только в крайних случаях и с параметром --loop) Небольшие изменения в логгировании Использование библиотеки pathlib вместо os.path

--- TG_AutoPoster/__main__.py
+++ TG_AutoPoster/__main__.py
@@ -1,14 +1,17 @@
-import sys
-from .TG_AutoPoster import create_parser, log, AutoPoster
+from loguru import logger as log
+from .TG_AutoPoster import AutoPoster, create_parser
 
 
 if __name__ == "__main__":
     args = create_parser().parse_args()
     if args.debug:
-        log.add(sys.stdout, colorize=True)
         log.add("./logs/bot_log_DEBUG.log", level="DEBUG", rotation="5 MB")
+    else:
+        log.remove()
+        log.add("./logs/bot_log_{time}.log")
     log.info("Начало работы.")
     autoposter = AutoPoster(config_path=args.config, cache_dir=args.cache_dir, ipv6=args.ipv6)
+    autoposter.IGNORE_ERRORS = args.ignore_errors
     if args.loop or args.sleep:
         sleep_time = args.sleep if args.sleep else 3600
         log.info("Программе был передан аргумен --loop (-l). Запуск бота в бесконечном цикле.")
