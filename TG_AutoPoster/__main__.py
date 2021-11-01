1d75f98ff9a104667b58465ddc5eb4689bc94932
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2021-11-01 23:13:25 +0300

    Возможность использовать переменные окружения вместо параметров --loop и --sleep Использование vk_api 11.9.5

--- TG_AutoPoster/__main__.py
+++ TG_AutoPoster/__main__.py
@@ -1,5 +1,6 @@
 from loguru import logger as log
 from .TG_AutoPoster import AutoPoster, create_parser
+import os
 
 
 if __name__ == "__main__":
@@ -10,11 +11,29 @@ if __name__ == "__main__":
         log.remove()
         log.add("./logs/bot_log_{time}.log")
     log.info("Начало работы.")
-    autoposter = AutoPoster(config_path=args.config, cache_dir=args.cache_dir, ipv6=args.ipv6)
-    autoposter.IGNORE_ERRORS = args.ignore_errors
+    autoposter = AutoPoster(
+        config_path=args.config, cache_dir=args.cache_dir, ipv6=args.ipv6
+    )
     if args.loop or args.sleep:
         sleep_time = args.sleep if args.sleep else 3600
-        log.info("Программе был передан аргумент --loop (-l). Запуск бота в бесконечном цикле.")
+        log.info(
+            "Программе был передан аргумент --loop (-l). "
+            "Запуск бота в бесконечном цикле."
+        )
+        autoposter.infinity_run(interval=sleep_time)
+    elif os.getenv("TG_SLEEP") or os.getenv("TG_LOOP"):
+        try:
+            sleep_time = int(os.getenv("TG_SLEEP"))
+        except ValueError:
+            log.warning(
+                "Переменная окружения TG_SLEEP задана неверно, "
+                "используется значение 3600"
+            )
+            sleep_time = 3600
+        log.info(
+            "При запуске программы была найдена переменная окружения TG_LOOP. "
+            "Запуск бота в бесконечном цикле."
+        )
         autoposter.infinity_run(interval=sleep_time)
     else:
         autoposter.run()
