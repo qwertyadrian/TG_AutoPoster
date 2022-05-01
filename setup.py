ddbdf41ee3adfe864064e24e6bc062ab2e999f4b
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2022-05-01 20:03:17 +0300

    Подготовка к выпуску 2.4.3 Переход на pyrogram версии 2.0 Исправлена ошибка из-за которой отправлялись только вложения без подписи Исправлена опечатка в файле Docker.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = "2.4.2"
+version = "2.4.3"
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -28,14 +28,14 @@ setup(
 
     packages=find_packages(),
     install_requires=[
-        "pyrogram==1.4.8",
+        "pyrogram==2.0.16",
         "tgcrypto",
         "loguru==0.6.0",
         "wget==3.2",
         "mutagen==1.45.1",
-        "beautifulsoup4==4.10.0",
+        "beautifulsoup4==4.11.1",
         "vk_api==11.9.7",
-        "streamlink==3.2.0",
+        "streamlink==4.0.0",
     ],
 
     classifiers=[
@@ -43,10 +43,10 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Communications :: Chat",
     ]
