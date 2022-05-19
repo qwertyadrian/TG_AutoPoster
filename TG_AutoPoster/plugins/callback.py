66e8998d5543b3749d0d0747d45189bd819a450a
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2022-05-20 00:39:50 +0300

    Перенос функционала бота TG_AutoConfigurator

---
+++ TG_AutoPoster/plugins/callback.py
@@ -0,0 +1,122 @@
+from configparser import NoSectionError
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from .. import AutoPoster
+from ..utils import messages, tools
+
+
+@AutoPoster.on_callback_query()
+def callback(bot: AutoPoster, callback_query: CallbackQuery):
+    if tools.admin_check(bot, callback_query):
+        data = callback_query.data.split()
+        if data[0] == "delete":
+            bot.reload_config()
+            try:
+                section = {
+                    **dict(last_id=0, last_story_id=0, pinned_id=0),
+                    **bot.config["domains"].pop(data[1]),
+                }
+            except NoSectionError:
+                info = "Источник {} не был найден. Возможно он был уже удален.".format(
+                    data[1]
+                )
+            else:
+                info = messages.SECTION_DELETED.format(data[1], **section)
+            bot.save_config()
+            callback_query.edit_message_text(info)
+            return
+
+        elif data[0] == "switch":
+            bot.reload_config()
+            global_ = bot.config.get("settings", {}).get(
+                data[2],
+                True
+                if data[2] in ("disable_web_page_preview", "sign_posts")
+                else False,
+            )
+            if data[1] == "global":
+                bot.config["settings"][data[2]] = not global_
+            else:
+                local = bot.config["domains"][data[1]].get(data[2], global_)
+                if not local == global_:
+                    if data[2] in bot.config["domains"][data[1]].keys():
+                        bot.config["domains"][data[1]].pop(data[2])
+                else:
+                    bot.config["domains"][data[1]][data[2]] = not local
+            bot.save_config()
+            info, reply_markup = tools.generate_setting_info(bot, data[1])
+            callback_query.edit_message_text(
+                info, reply_markup=reply_markup, disable_web_page_preview=True
+            )
+            return
+
+        elif data[0] == "show":
+            bot.reload_config()
+            if data[2] == "send_reposts":
+                info = "**Настройка отправки репостов:**\n\n"
+                button_list = [
+                    InlineKeyboardButton(
+                        "Отключить", callback_data="reposts {} 0".format(data[1])
+                    ),
+                    InlineKeyboardButton(
+                        "Включить", callback_data="reposts {} 1".format(data[1])
+                    ),
+                ]
+                footer_buttons = [
+                    InlineKeyboardButton(
+                        "Только посты",
+                        callback_data="reposts {} post_only".format(data[1]),
+                    )
+                ]
+                button_list = tools.build_menu(
+                    button_list, n_cols=2, footer_buttons=footer_buttons
+                )
+
+                option = bot.config["settings"]["send_reposts"]
+
+                if data[1] != "global":
+                    button_list.append(
+                        [
+                            InlineKeyboardButton(
+                                "Использовать глобальное значение",
+                                callback_data="reposts {} reset".format(data[1]),
+                            )
+                        ]
+                    )
+
+                if data[1] != "global":
+                    if "send_reposts" in bot.config["domains"][data[1]].keys():
+                        option = bot.config["domains"][data[1]].get("send_reposts")
+                    else:
+                        info = messages.SOURCE_USE_GLOBAL_SETTINGS
+                if option in ("no", False):
+                    info += "Отправка репостов отключена"
+                elif option == "post_only":
+                    info += "Отправка только постов" + messages.PARTIAL_REPOSTS
+                elif option in ("all", True):
+                    info += "Отправка репостов включена"
+                reply_markup = InlineKeyboardMarkup(button_list)
+                callback_query.edit_message_text(info, reply_markup=reply_markup)
+                return
+
+        elif data[0] == "reposts":
+            value = bool(int(data[2])) if data[2].isdigit() else data[2]
+            if data[1] == "global":
+                bot.config["settings"]["send_reposts"] = value
+            else:
+                if (
+                    data[2] == "reset"
+                    or bot.config["settings"]["send_reposts"] == value
+                ):
+                    if "send_reposts" in bot.config["domains"][data[1]].keys():
+                        bot.config["domains"][data[1]].pop("send_reposts")
+                else:
+                    bot.config["domains"][data[1]]["send_reposts"] = value
+
+            bot.save_config()
+
+        info, reply_markup = tools.generate_setting_info(bot, data[1])
+        callback_query.edit_message_text(
+            info, reply_markup=reply_markup, disable_web_page_preview=True
+        )
---
+++ TG_AutoPoster/plugins/commands.py
@@ -0,0 +1,175 @@
+from random import choice
+
+import pyrogram.filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+
+from .. import AutoPoster
+from ..utils import messages, tools
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["start", "help"]) & pyrogram.filters.private
+)
+def send_welcome(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        button = [
+            [
+                InlineKeyboardButton(
+                    "Поиск среди источников", switch_inline_query_current_chat=""
+                )
+            ]
+        ]
+        message.reply(
+            messages.HELP.format(bot.get_me().username),
+            parse_mode=ParseMode.HTML,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(button),
+        )
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands="get_full_logs") & pyrogram.filters.private
+)
+def send_full_logs(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        logs = sorted(list(bot.logs_path.iterdir()))
+        if logs:
+            a = message.reply("Отправка логов...")
+            try:
+                a.reply_document(logs[-1])
+            except ValueError:
+                a.edit("Последний лог файл пустой")
+        else:
+            message.reply("Логи не найдены.")
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands="get_last_logs") & pyrogram.filters.private
+)
+def send_last_logs(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        logs = sorted(list(bot.logs_path.iterdir()))[-1]
+        try:
+            lines = int(message.command[1])
+        except (ValueError, IndexError):
+            lines = 15
+        if logs:
+            with logs.open() as f:
+                last_logs = "".join(f.readlines()[-lines:])
+                last_logs = (
+                    "Последние {} строк логов:\n\n".format(str(lines)) + last_logs
+                )
+            for msg in tools.split(last_logs):
+                message.reply(msg, parse_mode=ParseMode.DISABLED)
+        else:
+            message.reply("Логи не найдены.")
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["list", "sources_list"])
+    & pyrogram.filters.private
+)
+def source_list(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        bot.reload_config()
+        sources_list = bot.config["domains"].keys()
+        sources = "Список источников:\nИсточник        ---->        Назначение  (ID последнего отправленного поста)\n\n"
+        for source in sources_list:
+            sources += "https://vk.com/{}        ---->        {}  ({})\n".format(
+                source,
+                bot.config["domains"][source]["channel"],
+                bot.config["domains"][source]["last_id"],
+            )
+        sources += (
+            "\nДля удаления источника отправьте команду /remove <домен группы вк>\nНапример, /remove "
+            + choice(list(sources_list))
+        )
+        message.reply(
+            sources, disable_web_page_preview=True, parse_mode=ParseMode.DISABLED
+        )
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(
+        commands=["remove_source", "remove", "delete", "delete_source"]
+    )
+    & pyrogram.filters.private
+)
+def remove_source(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        if len(message.command) > 1:
+            bot.reload_config()
+            section = {
+                **dict(last_id=0, last_story_id=0, pinned_id=0),
+                **bot.config["domains"].pop(message.command[1]),
+            }
+            bot.save_config()
+            info = messages.SECTION_DELETED.format(message.command[1], **section)
+            message.reply(info)
+        else:
+            message.reply(messages.REMOVE)
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["add"]) & pyrogram.filters.private
+)
+def add_source(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        if len(message.command) >= 3:
+            bot.reload_config()
+            bot.config["domains"][message.command[1]] = dict(
+                zip(
+                    ["channel", "last_id", "pinned_id", "last_story_id"],
+                    [int(i) if i.isnumeric() else i for i in message.command[2:]],
+                )
+            )
+            info = "Источник {} был добавлен.".format(message.command[1])
+            bot.save_config()
+            message.reply(info)
+        else:
+            message.reply(messages.ADD, parse_mode=ParseMode.MARKDOWN)
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["settings"]) & pyrogram.filters.private
+)
+def settings(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        bot.reload_config()
+        info, reply_markup = tools.generate_setting_info(bot, "global")
+        message.reply(info, reply_markup=reply_markup)
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["get_config"]) & pyrogram.filters.private
+)
+def get_config(bot: AutoPoster, message: Message):
+    if tools.admin_check(bot, message):
+        message.reply(
+            "Конфигурация бота:\n```{}```".format(bot.config_path.read_text())
+        )
+        message.reply_document(
+            document=bot.config_path, caption="Файл конфигурации бота."
+        )
+
+
+@AutoPoster.on_message(
+    pyrogram.filters.command(commands=["about"]) & pyrogram.filters.private
+)
+def about(_, message: Message):
+    message.reply(messages.ABOUT, disable_web_page_preview=True)
+
+
+@AutoPoster.on_message(pyrogram.filters.command(commands=["get_id"]))
+def get_id(_, message: Message):
+    message.reply("Chat id is `{}`".format(message.chat.id))
+
+
+@AutoPoster.on_message(pyrogram.filters.forwarded)
+def get_forward_id(_, message: Message):
+    if message.forward_from:
+        id_ = message.forward_from.id
+    else:
+        id_ = message.forward_from_chat.id
+    message.reply("Channel (user) ID is `{}`".format(id_))
