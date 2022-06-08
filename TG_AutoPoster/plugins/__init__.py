6f8496a2dacafdb1ad308882f29ccc8c05ea8aee
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2022-06-08 19:12:44 +0300

    Исправлена ошибка импорта

--- TG_AutoPoster/plugins/__init__.py
+++ TG_AutoPoster/plugins/__init__.py
@@ -1,5 +1,5 @@
 from .callback import callback
 from .commands import (about, add_source, get_config, get_forward_id, get_id, register,
                        remove_source, send_full_logs, send_last_logs, send_welcome,
-                       settings, source_list)
+                       settings)
 from .inline import inline
