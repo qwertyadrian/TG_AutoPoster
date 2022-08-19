a7476f92e45b2120eb8bb8b1d99b77d52b442d7f
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2022-08-19 19:45:14 +0300

    TG_AutoPoster 3.3.0b1

--- TG_AutoPoster/__init__.py
+++ TG_AutoPoster/__init__.py
@@ -1,7 +1,7 @@
 from .TG_AutoPoster import AutoPoster
 
 __author__ = "Adrian Polyakov (me@qwertyadrian.ru)"
-__version__ = "3.2.0"
+__version__ = "3.3.0b1"
 __copyright__ = "Copyright (c) 2018-2022 Adrian Polyakov"
 __license__ = "MIT"
 
