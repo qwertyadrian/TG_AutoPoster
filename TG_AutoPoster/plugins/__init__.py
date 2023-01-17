2af129241f4ed15a34371e41f10e1ce58900bd4a
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2023-01-17 17:22:12 +0300

    Добавлена команда `/cancel` для отмены операции

--- TG_AutoPoster/plugins/__init__.py
+++ TG_AutoPoster/plugins/__init__.py
@@ -1,6 +1,6 @@
 from .callback import (delete_domain, reposts_config, show_option,
                        switch_option, wts_config, set_param)
-from .commands import (about, add_source, exit_, get_config, get_forward_id,
+from .commands import (about, add_source, cancel, exit_, get_config, get_forward_id,
                        get_id, register, remove_source, restart,
                        send_full_logs, send_last_logs, send_welcome, settings,
                        update_header_footer)
