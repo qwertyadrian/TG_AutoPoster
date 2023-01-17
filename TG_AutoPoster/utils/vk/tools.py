0d8fff53aa1c1bf256272822af69bf1e1f2453c5
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2023-01-17 16:58:04 +0300

    Использование собственного конвертера MPEG TS в MP3 вместо streamlink и ffmpeg

--- TG_AutoPoster/utils/vk/tools.py
+++ TG_AutoPoster/utils/vk/tools.py
@@ -1,8 +1,12 @@
+import binascii
+import os
 import re
-import subprocess
-import time
-from typing import List
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
 
+import m3u8
+from Crypto.Cipher import AES
+from moviepy.audio.io.AudioFileClip import AudioFileClip
 from mutagen.id3 import APIC, ID3, TIT2, TPE1, error
 from mutagen.mp3 import MP3
 from requests import Session
@@ -31,13 +35,6 @@ class Attachments:
             return self.documents
         else:
             raise KeyError(f"Key {item} not found")
-
-
-def start_process(command: List) -> int:
-    process = subprocess.Popen(command)
-    while process.poll() is None:
-        time.sleep(1)
-    return process.returncode
 
 
 def add_audio_tags(filename, artist, title, track_cover):
@@ -80,3 +77,57 @@ def download_video(session: Session, link: str) -> str:
         for chunk in filereq:
             f.write(chunk)
     return file
+
+
+def get_key(data):
+    for i in range(data.media_sequence):
+        try:
+            key_uri = data.keys[i].uri
+            host_uri = "/".join(key_uri.split("/")[:-1])
+            return host_uri
+        except Exception:
+            continue
+
+
+def read_keys(path):
+    data_response = urlopen(path)
+    content = data_response.read()
+    return content
+
+
+def get_ts(url):
+    data = m3u8.load(url)
+    key_link = get_key(data)
+    ts_content = b""
+    key = None
+
+    for i, segment in enumerate(data.segments):
+        decrypt_func = lambda x: x
+        if segment.key.method == "AES-128":
+            if not key:
+                key_uri = segment.key.uri
+                key = read_keys(key_uri)
+            ind = i + data.media_sequence
+            iv = binascii.a2b_hex("%032x" % ind)
+            cipher = AES.new(key, AES.MODE_CBC, iv=iv)
+            decrypt_func = cipher.decrypt
+
+        ts_url = f"{key_link}/{segment.uri}"
+        coded_data = read_keys(ts_url)
+        ts_content += decrypt_func(coded_data)
+    return ts_content
+
+
+def m3u8_to_mp3(url, name):
+    ts_content = get_ts(url)
+    if ts_content is None:
+        raise TypeError("Empty mp3 content to save.")
+
+    tmp_file = NamedTemporaryFile(delete=False, suffix=".mp3")
+    tmp_file.write(ts_content)
+    tmp_file.close()
+
+    audioclip = AudioFileClip(tmp_file.name)
+    audioclip.write_audiofile(name)
+    audioclip.close()
+    os.unlink(tmp_file.name)
