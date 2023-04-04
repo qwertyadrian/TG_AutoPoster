d43a1fdbe5422c9d13f3e85c720380635b8c9d78
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2023-04-04 23:19:05 +0300

    Добавлены команды `/delete_blacklist` и `/delete_stoplist` для удаления черного списка и списка стоп-слов соответственно (#18)

--- TG_AutoPoster/plugins/__init__.py
+++ TG_AutoPoster/plugins/__init__.py
@@ -3,6 +3,6 @@ from .callback import (delete_domain, reposts_config, show_option,
 from .commands import (about, add_source, cancel, exit_, get_config,
                        get_id, register, remove_source, restart,
                        send_full_logs, send_last_logs, send_welcome, settings,
-                       update_blacklist, update_stoplist)
+                       update_blacklist, update_stoplist, delete_stoplist)
 from .handlers import update_header_footer, get_forward_id, stoplist_update
 from .inline import inline
