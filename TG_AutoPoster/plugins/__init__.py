df85135dfbbff3472fbb3921e723fd105f45b353
qwertyadrian <20292956+qwertyadrian@users.noreply.github.com>
2023-07-10 21:55:34 +0300

    Добавлена команда `/check` для проверки доступности источников (#27)

--- TG_AutoPoster/plugins/__init__.py
+++ TG_AutoPoster/plugins/__init__.py
@@ -1,6 +1,6 @@
 from .callback import (delete_domain, reposts_config, show_option,
                        switch_option, wts_config, set_param)
-from .commands import (about, add_source, cancel, exit_, get_config,
+from .commands import (about, add_source, cancel, check, exit_, get_config,
                        get_id, register, remove_source, restart,
                        send_full_logs, send_last_logs, send_welcome, settings,
                        update_blacklist, update_stoplist, delete_stoplist)
